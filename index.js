73ed9aec6c74773181aa7a3786fb0c225ad987c6
canadaservice <serviceprive93@gmail.com>
2026-04-29 09:29:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const axios = require("axios");
 const crypto = require("crypto");
 const cors = require("cors");
-
 const app = express();
 
 // ✅ CORS (OBLIGATOIRE)
